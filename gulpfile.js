a7ba45bbac6a2b8ad31c64232977f7d3742a9a95
Anna <a.chaikalemberg@gmail.com>
2017-06-21 15:23:29 +0300

    adds final version of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,94 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
-    concat = require('gulp-concat-css');
-var del = require('del');
-var copy  = require('gulp-copy');
-var destination = 'dest';
-var cssnano = require('gulp-cssnano');
-var jshint = require('gulp-jshint');
+/* requirements */
 
-gulp.task('copy', function() {
-    var sourceFiles = ['dev/images/*.png', 'dev/fonts/*.*', 'dev/*.html', 'dev/styles/*.css'];
-  return gulp
-    .src(sourceFiles)
-    .pipe(copy (destination, { prefix: 1 }))
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync').create(),
+  concat = require('gulp-concat-css'),
+  del = require('del'),
+  copy = require('gulp-copy'),
+  destination = 'dist',
+  cssnano = require('gulp-cssnano'),
+  jshint = require('gulp-jshint'),
+  prefixer = require('gulp-autoprefixer'),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify');
+
+
+// ----- dev-tasks
+
+
+// compils sass to css and adds vendor prefixes 
+gulp.task('sass', () => {
+  return gulp.src('dev/styles/*.sass')
+    .pipe(sass())
+    .pipe(prefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dev/styles'))
 });
 
 
-gulp.task('del', function() {
+// looks for errors and warnings and compiles .js to es5
+gulp.task('js', () => {
+  return gulp.src('es6/script.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dev/scripts/script.js'));
+});
+
+// reloads browser-sync on change of html, css, js files 
+gulp.task('browse', ['sass'], () => {
+  browserSync.init({
+    server: {
+      baseDir: 'dev'
+    },
+    notify: false
+  });
+
+  gulp.watch('dev/styles/*.sass', ['sass']);
+  gulp.watch('dev/es6/*.js'), ['js'];
+  browserSync.watch(['dev/*.html', 'dev/styles/*.css', 'scripts/*.js']).on('change', browserSync.reload);
+});
+
+
+// prepares files for release
+
+// cleans dir for release
+gulp.task('del', () => {
   return del(destination)
 });
 
-gulp.task('sass', function(){
-	return gulp.src('dev/styles/*.sass')
-	.pipe(sass())
-	.pipe(gulp.dest('dev/styles'))
-//    .pipe(minifyCSS())
-//	.pipe(browserSync.stream())
+// simply copies files, that don't need additional processing
+
+gulp.task('copy', () => {
+  var sourceFiles = ['dev/images/*.png', 'dev/fonts/*.*', 'dev/*.html'];
+  return gulp
+    .src(sourceFiles)
+    .pipe(copy(destination, {
+      prefix: 1
+    }))
 });
 
-gulp.task('browse', ['sass'], function() {
-	browserSync.init({
-		server: {
-          baseDir: 'dev'
-        },
-      notify: false
-	});
-	
-	gulp.watch('dev/styles/*.sass', ['sass']);
-	   browserSync.watch(['dev/*.html', 'dev/styles/*.css', 'scripts/*.js']).on('change', browserSync.reload);
-});
-
+// gathers all css to one file and minifies them 
 gulp.task('css-prod', function() {
-  return gulp.src('dest/styles/*.css')
-  .pipe(concat('style.css'))
-  .pipe(cssnano())
-  .pipe(gulp.dest('dest/styles'));
+  return gulp.src('dev/styles/*.css')
+    .pipe(concat('style.css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/styles'));
 })
+
+// gathers all js files (already babelified) to one file and minifies them 
+gulp.task('js-prod', () => {
+  return gulp.src(['dev/scripts/*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+})
+
 
 gulp.task('default', ['browse']);
 //
-gulp.task('prod', ['del', 'copy']);
+gulp.task('prod', ['del', 'copy', 'css-prod', 'js-prod']);
